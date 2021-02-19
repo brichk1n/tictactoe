d4af1fcfff99459a0dfaf305c4f0bf1a85bdc7ca
GavrWampus <gavriilganesh@gmail.com>
2021-02-19 23:07:29 +0300

    Cleaning branch
    All files from "master" has been deleted.

--- saved.lua
+++
@@ -1,8 +0,0 @@
-local composer = require("composer")
-
-local scene = composer.newScene()
-
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
